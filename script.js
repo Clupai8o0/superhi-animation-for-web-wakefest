d9db8458188b0f00174e912eb205c85fd783b89a
Clupai8o0 <clupaio4@gmail.com>
2022-07-31 14:48:12 +0300

    adding tween factor

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ mainTag.style.left = "0";
 mainTag.style.width = "100%";
 
 let currentScroll = 0;
+let aimScroll = 0;
 
 const changeScroll = function () {
 	bodyTag.style.height = mainTag.offsetHeight + "px";
+	currentScroll = currentScroll + (aimScroll - currentScroll) * 0.05;
+
 	mainTag.style.top = -1 * currentScroll + "px";
 
 	requestAnimationFrame(changeScroll);
 };
 
 window.addEventListener("scroll", () => {
-  currentScroll += window.scrollY;
-})
+	aimScroll += window.scrollY;
+});
 
 changeScroll();
