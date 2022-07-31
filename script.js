d648e9e985a635e69dc6848b35f0c49f06d0ab3f
Clupai8o0 <clupaio4@gmail.com>
2022-07-31 14:45:45 +0300

    replicating normal scroll

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mainTag = document.querySelector("main");
+const bodyTag = document.querySelector("body");
 
 mainTag.style.position = "fixed";
 mainTag.style.top = "0";
@@ -8,10 +9,14 @@ mainTag.style.width = "100%";
 let currentScroll = 0;
 
 const changeScroll = function () {
-	currentScroll += 1;
+	bodyTag.style.height = mainTag.offsetHeight + "px";
 	mainTag.style.top = -1 * currentScroll + "px";
 
 	requestAnimationFrame(changeScroll);
 };
+
+window.addEventListener("scroll", () => {
+  currentScroll += window.scrollY;
+})
 
 changeScroll();
